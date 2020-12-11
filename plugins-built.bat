686188503c110f786f504043630551b4d16cc0c4
kafka1408me <7mafia7@bk.ru>
2020-12-11 20:22:35 +0300

    remove " from bat

--- plugins-built.bat
+++ plugins-built.bat
@@ -1,4 +1,4 @@
 set platforms=%1\platforms
 set sqldrivers=%1\sqldrivers
-if not exist %platforms% mkdir "%platforms%
-if not exist %sqldrivers% mkdir "%sqldrivers%
+if not exist %platforms% mkdir %platforms%
+if not exist %sqldrivers% mkdir %sqldrivers%
